508bf6478c939dc4795a9fcc3ad426995151d0c4
henri <ahenrij@gmail.com>
2021-03-31 23:54:06 +0200

    tag res. reviewed

--- src/main/java/fr/istic/taa/jaxrs/rest/TagResource.java
+++ src/main/java/fr/istic/taa/jaxrs/rest/TagResource.java
@@ -4,7 +4,10 @@ import fr.istic.taa.jaxrs.dao.TagDao;
 import fr.istic.taa.jaxrs.dao.UserDao;
 import fr.istic.taa.jaxrs.domain.Tag;
 import fr.istic.taa.jaxrs.domain.User;
+import fr.istic.taa.jaxrs.dto.TagDto;
+import fr.istic.taa.jaxrs.dto.mappers.TagMapper;
 import fr.istic.taa.jaxrs.utils.Secured;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 
 import javax.inject.Inject;
@@ -26,10 +29,11 @@ public class TagResource {
 
     @POST
     @Consumes("application/json")
+    @Operation(summary = "Create a Tag.")
     public Response createTag(@Context SecurityContext securityContext, @Parameter(description = "Tag object to save", required = true) Tag tag) {
         try {
             String userId = securityContext.getUserPrincipal().getName();
-            tag.setOwner(userDao.findOne(Long.valueOf(userId)));
+            tag.setOwner(userDao.getReference(Long.valueOf(userId)));
             tagDao.save(tag);
             return Response.ok().entity(tag).build();
         } catch (Exception e) {
@@ -38,6 +42,7 @@ public class TagResource {
     }
 
     @GET
+    @Operation(summary = "Get Tags create by current user.")
     public Response getTags(@Context SecurityContext securityContext) {
         try {
             String userId = securityContext.getUserPrincipal().getName();
@@ -50,12 +55,12 @@ public class TagResource {
 
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response updateTag(@Parameter(description = "Tag to update") Tag tag) {
+    @Operation(summary = "Update Tag's attributes.")
+    public Response updateTag(@Parameter(description = "Tag to update") TagDto tagDto) {
         try {
-            Tag tagOld = tagDao.findOne(tag.getId());
-            tagOld.setName(tag.getName());
-            tagOld.setColor(tag.getColor());
-            tagDao.update(tagOld);
+            Tag tag = tagDao.findOne(tagDto.getId());
+            TagMapper.INSTANCE.updateAttrs(tagDto, tag);
+            tagDao.update(tag);
             return Response.ok().entity(tag).build();
         } catch (Exception e) {
             return Response.status(Response.Status.BAD_REQUEST).entity("Something went wrong: " + e.getMessage()).build();
@@ -64,6 +69,7 @@ public class TagResource {
 
     @DELETE
     @Path("/{id}")
+    @Operation(summary = "Delete a Tag.")
     public Response deleteTag(@Context SecurityContext securityContext, @PathParam("id") Long tagId) {
         try {
             Tag tag = tagDao.getReference(tagId);
